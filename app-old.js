7aea02fcdab19b1ce4f1e76c05258dc1a3ff15ec
Gustavo <belnem88@gmail.com>
2019-06-25 20:43:59 -0300

    primer commit

---
+++ app-old.js
@@ -0,0 +1,18 @@
+const htpp = require('http');
+
+
+htpp.createServer((req, res) => {
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        let salida = {
+            nombre: 'Gustavo',
+            edad: 30,
+            url: req.url
+        }
+        res.write(JSON.stringify(salida))
+            //res.write('Hola mundo');
+        res.end();
+    })
+    .listen(8080);
+
+console.log('Escuchando el puerto 8080');
